4ceb3a97530389e5c9b41b161af12305f34d3acf
LoYuSum <loyusum246807952@gmail.com>
2020-06-07 21:42:50 +0800

    apply ui and able to collect user name and password

--- Interface/Window/UserLoginWindow.cpp
+++ Interface/Window/UserLoginWindow.cpp
@@ -4,10 +4,30 @@
 
 #include "UserLoginWindow.hpp"
 
+const std::string UserLoginWindow::sm_font_path = "resource/ShareTechMono.ttf";
+
 UserLoginWindow::UserLoginWindow(int screen_size_x, int screen_size_y) {
 
     m_app = ToolBox::make_unique<sf::RenderWindow>(sf::VideoMode(screen_size_x, screen_size_y),
-                                                   "EMCMeeting", sf::Style::Default - sf::Style::Resize);
+                                                   "Login", sf::Style::Default - sf::Style::Resize);
+
+    if (!TextRenderer::addFont(sm_font_path, "default")) {
+
+        ToolBox::err() << "Can't load font : " << sm_font_path << std::endl;
+    }
+
+    // set the input layout
+    m_input_fields.emplace_back("user_name_id", "default", "user name",
+                                50, 10, 300, 40, 30);
+
+    m_input_fields.emplace_back("password_id", "default", "password",
+                                50, 70, 300, 40, 30);
+
+    // set default user name and password
+    user_name = m_input_fields[0].getText();
+    password = m_input_fields[1].getText();
+
+    m_buttons = ToolBox::make_unique<Button>("ok", "default", "Login", 100, 140, 200, 40, 25);
 }
 
 bool UserLoginWindow::Update() {
@@ -23,12 +43,12 @@ bool UserLoginWindow::Update() {
 
                 case sf::Event::Closed:
 
-                    m_app->close();
-                    break;
+                    Close();
+                    return false;
 
                 case sf::Event::TextEntered:
 
-
+                    text_input(app_event.text.unicode);
 
                     break;
 
@@ -39,10 +59,47 @@ bool UserLoginWindow::Update() {
 
         }
 
-        m_app->clear();
+        m_app->clear(sf::Color(0, 152, 152));
+
+        m_buttons->display(m_app.get());
+
+        if (m_buttons->isPressed()) {
+
+            Close();
+            return false;
+        }
+
+        for (auto &field : m_input_fields) {
+            field.display(m_app.get(), clock());
+            sf::Text a;
+            a.setCharacterSize(1);
+        }
+
         m_app->display();
 
     }
 
     return is_open;
+}
+
+void UserLoginWindow::text_input(const char character) {
+
+    if (m_input_fields[0].isConcentrated()) {
+        m_input_fields[0].push_back(character);
+
+        user_name = m_input_fields[0].getText();
+    }
+
+    if (m_input_fields[1].isConcentrated()) {
+        m_input_fields[1].push_back(character);
+
+        password = m_input_fields[1].getText();
+    }
+}
+
+void UserLoginWindow::Close() {
+    user_name = m_input_fields[0].getText();
+    password = m_input_fields[1].getText();
+
+    m_app->close();
 }
--- Interface/Window/UserLoginWindow.hpp
+++ Interface/Window/UserLoginWindow.hpp
@@ -5,21 +5,37 @@
 #ifndef EMCMEETING_USERLOGINWINDOW_HPP
 #define EMCMEETING_USERLOGINWINDOW_HPP
 
+#include <ctime> //for clock()
 #include <string>
 #include <SFML/Graphics.hpp>
 
 #include "WindowBase.hpp"
 #include "../../ToolBox/ToolBox.hpp"
 
+#include "../UI/Button/Button.hpp"
+#include "../UI/InputField/InputField.hpp"
+
 class UserLoginWindow : public WindowBase {
 
+private:
+
+    static const std::string sm_font_path;
+
+
+    std::vector<InputField> m_input_fields;
+    std::unique_ptr<Button> m_buttons;
+
+    void text_input(const char character);
+
 public:
-    std::string user_name, password;
+    std::string user_name = "Empty", password = "Empty";
 
     UserLoginWindow(int screen_size_x, int screen_size_y);
 
     // return false if window has closed or user had completed to fill in their data
     bool Update() override;
+
+    void Close() override ;
 };
 
 
