37723f2c393971d5f0a5f525000b7a0fdc7b34f1
LoYuSum <loyusum246807952@gmail.com>
2020-06-07 17:34:43 +0800

    SFML graphic library added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 
+#include <SFML/Graphics.hpp>
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    sf::RenderWindow main_app(sf::VideoMode(800, 600), "EMCMeeting",
+                              sf::Style::Default - sf::Style::Resize);
+
+    while (main_app.isOpen()) {
+
+        sf::Event app_event{};
+
+        while (main_app.pollEvent(app_event)) {
+
+            switch (app_event.type) {
+
+                case sf::Event::Closed:
+
+                    main_app.close();
+                    break;
+
+                default:
+                    break;
+
+            }
+
+        }
+
+        main_app.clear();
+        main_app.display();
+
+    }
+
     return 0;
 }
