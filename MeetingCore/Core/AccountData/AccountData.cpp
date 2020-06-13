d542f7e6357ad04f8cb2610c2451f1abec86be13
LoYuSum <loyusum246807952@gmail.com>
2020-06-13 21:30:02 +0800

    login and log out moved to UserClient class, added meeting begin and end time save

--- MeetingCore/Core/AccountData/AccountData.cpp
+++ MeetingCore/Core/AccountData/AccountData.cpp
@@ -10,25 +10,8 @@ AccountData::AccountData() : AccountData("", "") {
 
 AccountData::AccountData(std::string user_name, std::string password) : m_user_name(user_name), m_password(password) {
 
-    ToolBox::log() << "Account created with user name [" << m_user_name << "] and password [" << m_password << "]" << std::endl;
-
-}
-
-bool AccountData::login() {
-    if (m_is_login) {
-        return true;
-    }
-
-#warning login not finished
-
-    return false;
-}
-
-void AccountData::logout() {
-
-#warning logout not finished
-
-    m_is_login = false;
+    ToolBox::log() << "Account created with user name [" << m_user_name << "] and password [" << m_password << "]"
+                   << std::endl;
 
 }
 
@@ -42,4 +25,8 @@ void AccountData::AddActiveTime(float time) {
 
     m_active_time += time;
 
+}
+
+float AccountData::GetFocusTime() {
+    return m_active_time;
 }
--- MeetingCore/Core/AccountData/AccountData.hpp
+++ MeetingCore/Core/AccountData/AccountData.hpp
@@ -7,10 +7,15 @@
 
 #include <iostream>
 #include <string>
+#include <chrono>
 
 #include "../../../ToolBox/ToolBox.hpp"
 
 class EMCMeeting;
+
+class DataCollector;
+
+class UserClient;
 
 class AccountData {
 
@@ -19,22 +24,34 @@ private:
     std::string m_user_name;
     std::string m_password;
 
+    typeof(std::chrono::system_clock::now()) m_begin_meeting_time, m_end_meeting_time;
+
+    /*
+     *
+     *  total time that the main window stay active ( concentrated ) since meeting start
+     *
+     */
     float m_active_time = 0.0f;
 
     bool m_is_login = false;
 
     void SetActiveTime(float time = 0);
+
     void AddActiveTime(float time);
 
 public:
 
     AccountData(std::string user_name, std::string password);
+
     AccountData();
 
-    bool login();
-    void logout();
+    float GetFocusTime();
 
     friend class EMCMeeting;
+
+    friend class DataCollector;
+
+    friend class UserClient;
 };
 
 
