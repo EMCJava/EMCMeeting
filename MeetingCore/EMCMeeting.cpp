58e3e1ccaaecb7983c2a88e03dde41fc233ee375
LoYuSum <loyusum246807952@gmail.com>
2020-06-13 22:01:54 +0800

    upload attention data when client quit, client connection, use ui to determine user type

--- MeetingCore/EMCMeeting.cpp
+++ MeetingCore/EMCMeeting.cpp
@@ -4,48 +4,258 @@
 
 #include "EMCMeeting.hpp"
 
-EMCMeeting::EMCMeeting() {
+EMCMeeting::EMCMeeting(int max_client) {
 
-    ToolBox::log() << "new EMCMeeting Created" << std::endl;
+    ToolBox::log() << "new EMCMeeting Created";
 
-    AccountLogIn();
+    UserSetup_(max_client);
 
     m_main_window = ToolBox::make_unique<Window>(false, 800, 600);
+
+    // reset the active timer once the window open
+    ResetFocusTimer_();
 
 }
 
 bool EMCMeeting::Update() {
 
+    // if user is a hoster of meeting
+    if (m_hoster_server) {
+
+        m_hoster_server->Update();
+
+    } else { // otherwise ...
+
+        UpdateUserAttentionData_(m_main_window->IsOnFocus());
+
+    }
+
     if (!m_main_window->Update()) {
-#warning[return false;] commented for testing without window
-        //ToolBox::err() << "Window was closed, unable to update" << std::endl;
-        //return false;
+#warning for server debug
+        if (m_hoster_server)
+            return true;
+
+        ToolBox::err() << "Window was closed, unable to update" << std::endl;
+
+        // if is a client
+        if (m_user_client) {
+
+            // upload any required data to hoster and exit
+            UploadUserAttentionData();
+
+        }
+
+        // tell user the main window has closed
+        return false;
     }
 
     return true;
 
 }
 
-bool EMCMeeting::AccountLogIn() {
-#warning It should implement in UI
-
+bool EMCMeeting::AccountLogIn_(bool *isCancel) {
     UserLoginWindow loginWindow(400, 200);
 
     // wait till window had closed, which means user has finished filling in their user data
     while (loginWindow.Update());
 
+    if(isCancel && loginWindow.IsCancel()){
+        *isCancel = true;
+
+        return false;
+    }
+
     // get the name and password from the input window class
     std::string username = loginWindow.user_name, password = loginWindow.password;
 
     m_user_account_data = ToolBox::make_unique<AccountData>(username, password);
+    m_user_account_data->m_begin_meeting_time = std::chrono::system_clock::now();
 
-    // trt to log in
-    if (!m_user_account_data->login()) {
+    // try to log in
+    if (!m_user_client->login(*m_user_account_data)) {
 
         ToolBox::err() << "Invalid user name or password, please check" << std::endl;
+        WinError_("Invalid user name or password, please check");
 
-        return true;
+        return false;
     }
 
     return true;
+}
+
+void EMCMeeting::UploadUserAttentionData() {
+
+    // is a client
+    if (m_user_client) {
+
+        // user attention timer last accumulate
+        UpdateUserAttentionData_(false);
+
+        m_user_account_data->m_end_meeting_time = std::chrono::system_clock::now();
+        m_user_client->SendAccountDataToServer_(*m_user_account_data);
+
+    }
+
+}
+
+void EMCMeeting::ResetFocusTimer_() {
+
+    m_last_time_since_on_focus = std::chrono::system_clock::now();
+
+}
+
+void EMCMeeting::UpdateUserAttentionData_(bool window_is_on_focus) {
+    if (window_is_on_focus) {
+
+        if (!m_window_on_focus) {
+
+            ResetFocusTimer_();
+
+        }
+
+        m_window_on_focus = true;
+
+    } else {
+
+        if (m_window_on_focus) {
+
+            // time passed since the last time we gain focus
+            const auto current_accumulated_focus_time = std::chrono::system_clock::now() - m_last_time_since_on_focus;
+
+            // reset timer
+            ResetFocusTimer_();
+
+            // time passed since the last time we gain focus in second (float form)
+            const auto time_passed = (float) std::chrono::duration_cast<std::chrono::microseconds>(
+                    current_accumulated_focus_time).count() * std::chrono::microseconds::period::num /
+                                     std::chrono::microseconds::period::den;
+
+            // accumulate attention time
+            m_user_account_data->AddActiveTime(time_passed);
+        }
+
+        m_window_on_focus = false;
+    }
+}
+
+UserAskWhetherHosterWindow::UserType EMCMeeting::SelectUserType_() {
+
+    UserAskWhetherHosterWindow win(400, 200);
+    while (win.Update());
+
+    return win.GetType();
+}
+
+std::pair<std::string, int> EMCMeeting::SelectIpAndPort_(bool *isCancel) {
+
+    std::unique_ptr<UserClientAskHosterIPAndPortWindow> win;
+
+    bool ip_accept = true;
+
+    do {
+
+        if (!ip_accept) {
+            WinError_("Ip address invalid");
+        }
+
+        win = ToolBox::make_unique<UserClientAskHosterIPAndPortWindow>(
+                400, 200);
+        while (win->Update());
+
+        if (isCancel) {
+
+            if (win->IsCancel()) {
+
+                *isCancel = true;
+                return {};
+            }
+        }
+
+        ToolBox::log() << "Connect to ip " << win->ip << ":" << win->port << std::endl;
+
+        ToolBox::log() << "IP " << ((ip_accept = Socket::IsValidIP(win->ip)) ? "valid" : "invalid") << std::endl;
+
+    } while (!ip_accept);
+
+    return {win->ip, atoi(win->port.c_str())};
+}
+
+bool EMCMeeting::ConnectToHoster_(bool *isCancel) {
+
+    bool has_connect = true;
+
+    do {
+
+        if (!has_connect) {
+
+            WinError_("Server no response");
+        }
+
+        bool is_cancel = false;
+        const auto addr = SelectIpAndPort_(&is_cancel);
+
+        if (isCancel && is_cancel) {
+
+            *isCancel = true;
+            return false;
+        }
+
+        m_user_client = ToolBox::make_unique<UserClient>(std::get<0>(addr), std::get<1>(addr));
+
+        has_connect = m_user_client->HasConnect();
+    } while (!has_connect);
+
+    bool is_cancel = false;
+    while (!AccountLogIn_(&is_cancel)){
+        if(is_cancel) {
+
+            m_user_client.reset();
+
+            // go back to previous stage (enter ip and port)
+            return ConnectToHoster_(isCancel);
+
+        }
+    }
+
+    return true;
+}
+
+void EMCMeeting::WinError_(std::string message, int weight, int height) {
+
+    ErrorWindow e_win(weight, height, message);
+    while (e_win.Update());
+
+}
+
+void EMCMeeting::UserSetup_(int max_client) {
+
+    const auto user_type = SelectUserType_();
+
+    if (user_type == UserAskWhetherHosterWindow::UserType::Client) {
+
+        ToolBox::log() << " as a Client" << std::endl;
+
+        bool is_cancel = false;
+        ConnectToHoster_(&is_cancel);
+
+        if (is_cancel) {
+
+            // select type again
+            UserSetup_();
+        }
+
+    } else if (user_type == UserAskWhetherHosterWindow::UserType::Hoster) {
+
+        ToolBox::log() << " as a Hoster" << std::endl;
+
+        m_hoster_server = ToolBox::make_unique<HosterServer>(max_client);
+        m_hoster_server->Start();
+
+    } else {
+
+        ToolBox::log() << std::endl << "No User Type selected, terminating process" << std::endl;
+        exit(0);
+
+    }
+
 }
--- MeetingCore/EMCMeeting.hpp
+++ MeetingCore/EMCMeeting.hpp
@@ -6,11 +6,21 @@
 #define EMCMEETING_EMCMEETING_HPP
 
 #include <iostream>
+#include <chrono> // for system clock
 #include <memory>
 
-#include "../Interface/Window/UserLoginWindow.hpp"
+#include "../Interface/ErrorWindow/ErrorWindow.hpp"
+#include "../Interface/UserClientAskHosterIPAndPortWindow/UserClientAskHosterIPAndPortWindow.hpp"
+#include "../Interface/UserAskWhetherHosterWindow/UserAskWhetherHosterWindow.hpp"
+#include "../Interface/UserLoginWindow/UserLoginWindow.hpp"
 #include "../Interface/Window/EMCWindow.hpp"
 #include "Core/AccountData/AccountData.hpp"
+
+#include "Core/AccountData/AccountData.hpp"
+
+//network
+#include "Core/Hoster/HosterServer/HosterServer.hpp"
+#include "Core/Client/UserClient.hpp"
 
 //for make_unique
 #include "../ToolBox/ToolBox.hpp"
@@ -24,19 +34,106 @@ private:
 
     /*
      *
+     * ptr of the HosterServer, if user is not a hoster, stay nullptr
+     *
+     */
+
+     std::unique_ptr<HosterServer> m_hoster_server;
+
+    /*
+    *
+    * ptr of the HosterServer, if user is not a hoster, stay nullptr
+    *
+    */
+
+    std::unique_ptr<UserClient> m_user_client;
+
+    /*
+     *
      * create a window for user to enter their name and password
      * return true if successfully login
      *
      */
 
-    bool AccountLogIn();
+    bool AccountLogIn_(bool *isCancel = nullptr);
+
+    /*
+     *
+     * create a window for user to select their user type [hoster / client]
+     * return the user type
+     *
+     */
+
+    UserAskWhetherHosterWindow::UserType SelectUserType_();
+
+    /*
+     *
+     * create a window for user to select the hoster ip and port
+     * return the [ip in string, port in integer]
+     *
+     */
+
+    std::pair<std::string, int> SelectIpAndPort_(bool *isCancel = nullptr);
+
+    /*
+     *
+     * create multiple window and process in order to let user login
+     * return true if successfully login
+     *
+     */
+
+    bool ConnectToHoster_(bool *isCancel = nullptr);
+
+    /*
+     *
+     * save the time the last time the window has the input focus
+     * used to send info to hoster
+     *
+     */
+
+    typeof(std::chrono::system_clock::now()) m_last_time_since_on_focus;
+
+    /*
+     *
+     * weather the main window has the input focus in last frame
+     *
+     */
+
+    bool m_window_on_focus = true;
+
+    /*
+     *
+     * set m_last_time_since_on_focus be std::chrono::system_clock::now()
+     *
+     */
+
+    void ResetFocusTimer_();
+
+    /*
+     *
+     *  Create a window to show error message
+     *
+     */
+
+    void WinError_(std::string message, int weight = 400, int height = 200);
+
+    void UserSetup_(int max_client = 1);
+
+    void UpdateUserAttentionData_(bool window_is_on_focus);
 
 public:
 
-    EMCMeeting();
+    explicit EMCMeeting(int max_client = 1);
 
     // return true if every things work well
     bool Update();
+
+    /*
+     *
+     * Upload the attention data to hoster if use is not
+     *
+     */
+    void UploadUserAttentionData();
 
 };
 
